de3fd01b02e36cf5ebef817c8cd2cff17ae75fc9
yogx <yogx4u@gmail.com>
2019-09-05 09:40:52 +0530

    First commit. Downloaded project from spring initializer.

---
+++ src/main/java/org/yogx/bootBoilerplate/BootBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package org.yogx.bootBoilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootBoilerplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/yogx/bootBoilerplate/BootBoilerplateApplicationTests.java
@@ -0,0 +1,16 @@
+package org.yogx.bootBoilerplate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootBoilerplateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
